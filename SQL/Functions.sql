b28a665c0d8387fb86e6e2e509eead3023d716b8
Thomas Farley <77354093+ThomasFarley13@users.noreply.github.com>
2021-12-13 19:03:58 -0500

    Update Functions.sql

--- SQL/Functions.sql
+++ SQL/Functions.sql
@@ -1,10 +1,10 @@
---this function will update the order_more boolean to true if we have less than 10 books in stock
+--this function will update the order_more boolean to true if we have less than 11 books in stock
 CREATE OR REPLACE FUNCTION buy_books() returns trigger as
 	$$
 	begin
 		UPDATE book 
 		SET order_more = TRUE
-		WHERE new.stock < 10;
+		WHERE new.stock < 11;
 	RETURN new;
 	end;
 	$$
