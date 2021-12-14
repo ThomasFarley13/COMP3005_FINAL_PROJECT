0026a34b96f23f0bc90b3007783f68fa6c5e7dd6
Thomas Farley <77354093+ThomasFarley13@users.noreply.github.com>
2021-12-13 19:04:44 -0500

    Update Triggers.sql

--- SQL/Triggers.sql
+++ SQL/Triggers.sql
@@ -1,6 +1,6 @@
---this triggers the buy_books() function if we have less than 10 books on update
+--this triggers the buy_books() function if we have less than 11 books on update
 CREATE TRIGGER book_order
 	AFTER UPDATE OF stock ON book
 	FOR EACH ROW
-	WHEN (new.stock < 10)
+	WHEN (new.stock < 11)
 	EXECUTE PROCEDURE buy_books();
